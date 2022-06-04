bfd14fa2801cbd2dfe1b58f749fac08233aad4b3
Teyllayka <teyylayt@gmail.com>
2022-06-04 14:57:55 +0300

    update

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-
-
-
 import os
 from os.path import isfile, join
 from time import sleep
 
-
 username = None
 
-def create_user():
+def register():
     usernames = []
-    with open('users.txt', "r") as f:
+    with open('users/users.txt', "r") as f:
         a = f.read().split()
         for i in range(len(a)):
             usernames.append(a[i].split(':')[0])
@@ -19,16 +15,18 @@ def create_user():
         print('already registred')
         username = input("enter an username: ")
     password = input("enter a password: ")
-    with open('users.txt', "a") as f:
+    with open('users/users.txt', "a") as f:
         f.write(f"{username}:{password}\n")
     print('user is created')
+    with open(f'users/{username}.txt', "a") as f:
+        f.write(f"{username}_file\n")
 
 
 def login():
     global username
     users = []
     passes = []
-    with open('users.txt', "r") as f:
+    with open('users/users.txt', "r") as f:
         a = f.read().split()
         for i in range(len(a)):
             users.append(a[i].split(':')[0])
@@ -149,13 +147,12 @@ def read_test():
         print(i)
     counter = int(input("select test "))
     while not os.path.isfile(f"test/test{counter}.txt"):
-        print('exercise not found')
-        counter = int(input("select exercise "))
+        print('test not found')
+        counter = int(input("select test "))
     questions = []
     answers = []
     with open(f"test/test{counter}.txt", "r") as f:
         a = f.read().split()
-        print(a)
         for i in range(len(a)-1):
             questions.append(a[i].split(':')[0])
             answers.append(a[i].split(':')[1])
@@ -169,6 +166,13 @@ def read_test():
         else:
             count = count
     print(f"you scored {count} points out of {max_answers}, your grade is {round(count/max_answers*10)}")
+    with open(f'users/{username}.txt', "r") as f:
+        read = f.read()
+        if f"test{counter}" in read:
+            pass
+        else:
+            with open(f'users/{username}.txt', "a") as a:
+                a.write(f"test{counter} completed: grade = {round(count/max_answers*10)}\n")
 
 
 
@@ -179,14 +183,14 @@ while True:
         try:
             a = int(input('do you want to login or register an user? (ctrl + c to exit) '))
             while a == 2:
-                create_user()
+                register()
                 a = int(input('do you want to login or register an user? (ctrl + c to exit) '))
             while a == 1:
                 error = login()
                 if error:
                     a = 3
                 else:
-                    a = int(input('do you want to login or register an user? (ctrl + c to exit) '))
+                    a = 0
         except ValueError:
             print('Value Error')
     print(f"currently logged in as {username}")
